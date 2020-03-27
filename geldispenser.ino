89e0f0dbcb53152926f954be127dc93fc10b780c
sunbowch <sunbow@bluewin.ch>
2020-03-27 21:18:35 +0100

    Update geldispenser.ino

--- geldispenser.ino
+++ geldispenser.ino
@@ -16,28 +16,28 @@ sparkfun distance sensor, a stepper motor and and optionnal fuse.
 The stepper runs a 3D printed peristaltic pump that gives the dose.
 
 */
-#include <SimpleSleep.h>
-#include <AccelStepper.h>
-#include "functions.h"
+#include <SimpleSleep.h>          //library to deep-sleep the mcu
+#include <AccelStepper.h>         //non-blocking stepper control library
+#include "functions.h"            //separate file to simplify the code
 
-#define motorInterfaceType 1
-#define dirPin 5
-#define stepPin 6
-#define enablePin 7
-#define trigPin 3
-#define echoPin 4
+#define motorInterfaceType 1          // see library definition, 1 = standard stepper driver
+#define dirPin 5                      // direction pin of the stepper driver
+#define stepPin 6                     // step pin 
+#define enablePin 7                   // power pin of the driver
+#define trigPin 3                     // trigger pin of the sensor, ask for a measure
+#define echoPin 4                     // input pin of the sensor data, pulse length proportional to distance
 #define onPin 2                       // VCC pin of the sensor
 #define stepperspeed 500              // Tune these parameters for your stepper and
-#define stepperacceleration 15000     // power supply
-#define stepsfordose 600              // tune for the desired quantity
+#define stepperacceleration 15000     // power supply to get smooth operation
+#define stepsfordose 600              // tune for the desired quantity of liquid
 #define backsteps 50                  // tune to avoid dripping
-#define minimumdistance 5             // to make the system react
-#define maximumdistance 25            // to make the system react
+#define minimumdistance 5             // to trigger the delivery
+#define maximumdistance 25            // to trigger the delivery
 
 AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, dirPin);
 distSensor sensor(trigPin,echoPin,onPin);
 SimpleSleep Sleep;
-int i;
+int i;                              // counts the loops when the hand is within reach
 
 void setup() {
 
@@ -50,7 +50,7 @@ void loop() {
   long dist;
   unsigned long runtime = millis();
 
-  sensor.powerOn();
+  sensor.powerOn();           // get the distance
   dist=sensor.echo();
 
   if (dist>=minimumdistance && dist <= maximumdistance){
@@ -65,28 +65,28 @@ void loop() {
     i=0;
     sensor.powerOff();
   }
-  Sleep.deeplyFor(400);         // So that the loop runs only every 0.4 seconds
+  Sleep.deeplyFor(400);         // leaves only a watchdog timer on So that the loop runs only every 0.4 seconds
 }
 
 void squirt(){
 
   long dist = sensor.echo();
   long position;
-  stepper.enableOutputs();      //powers on the stepper driver
+  stepper.enableOutputs();                //powers on the stepper driver
   stepper.setMaxSpeed(stepperspeed);
-  stepper.moveTo(stepsfordose);
+  stepper.moveTo(stepsfordose);           //define the movement to do
   unsigned long time= millis();
 
   do{
-    stepper.run();
+    stepper.run();              //moves the motor if a step is scheduled.
     if (millis()-time >= 100){  //check position every 1/10 seconds
       dist = sensor.echo();
       time=millis();
     }
   }
-  while (stepper.distanceToGo()!=0 && dist <maximumdistance );
+  while (stepper.distanceToGo()!=0 && dist <maximumdistance );      //runs until the dose is supplied or hand is removed
 
-  if (dist>=maximumdistance) i=0;
+  if (dist>=maximumdistance) i=0;                                   //resets the counter if the hand is removed
 
   position = stepper.currentPosition()-backsteps;
   stepper.runToNewPosition(position);
